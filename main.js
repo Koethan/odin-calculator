8144b9a44f7a99f87cfedc473d55b78818e772cb
Ethan Ospina <ethan_ospina@yahoo.com>
2024-01-30 14:14:21 -0500

    connected the operation logic to the buttons and got the display to show each number and total

--- main.js
+++ main.js
@@ -23,23 +23,65 @@ function divide(num1, num2){
 
 
 function calculator(num1, num2, operation){
-    if(operation === 'add'){
+    if(operation === '+'){
         return add(num1,num2)
     }
-    if(operation == 'subtract'){
+    if(operation == '-'){
         return subtract(num1,num2)
     }
-    if(operation == 'multiply'){
+    if(operation == '*'){
         return multiply(num1,num2)
     }
-    if(operation == 'divide'){
+    if(operation == '/'){
         return divide(num1,num2)
     }
 }
 
 // console.log(calculator(input1, input2, operation))
 
-let btn = document.querySelectorAll('button')
-console.log(btn)
+let digit = document.querySelectorAll('.number')
+let operator = document.querySelectorAll('.operation')
+let equals = document.querySelector('#equals')
+let clear = document.querySelector('#clear')
+console.log(digit, operator, equals, clear)
+let number = 0
+let firstNumber=0
+let secondNumber=0
+let operation;
 
-btn.
+let display = document.querySelector('#display')
+display.textContent = number
+digit.forEach((digit)=>{
+    digit.addEventListener('click',()=>{
+        
+        number += digit.innerText
+        console.log(digit.innerText)
+        display.textContent = number.slice(1)
+        if(operation == undefined){
+            firstNumber += digit.innerText
+        }else{
+            secondNumber += digit.innerText
+        }
+        
+    })
+})
+
+
+
+operator.forEach((op)=>{
+    op.addEventListener('click', ()=>{
+        operation = op.innerText
+        number = 0
+        display.textContent = number
+        console.log(operation)
+    })
+})
+
+let equalBtn = document.querySelector('#equals')
+equalBtn.addEventListener('click', ()=>{
+    let total = calculator(Number(firstNumber), Number(secondNumber), operation)
+    console.log(Number(firstNumber), Number(secondNumber), operation, total)
+    display.innerText = total
+})
+
+console.log(number)
